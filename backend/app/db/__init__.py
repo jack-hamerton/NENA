066ef53900dfd56e8e9ed991c3498a2ed2825bf2
hamertonotieno99 <hamertonotieno99@gmail.com>
2025-12-07 14:31:02 +0000

    83 commits

---
+++ backend/app/db/__init__.py
@@ -0,0 +1,9 @@
+from app.db.base_class import Base
+from app.models.user import User
+from app.models.post import Post
+from app.models.follower import Follower
+from app.models.collaboration import Collaboration
+from app.models.message import Message
+from app.models.comment import Comment
+from app.models.room import Room, RoomParticipant
+from app.models.study import Study, StudyQuestion, StudyResponse
---
+++ backend/app/models/user.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Boolean, Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from app.db.base_class import Base
+import datetime
+
+class User(Base):
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean(), default=True)
+    is_superuser = Column(Boolean(), default=False)
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    posts = relationship("Post", back_populates="owner")
+    followers = relationship("Follower", foreign_keys=["Follower.followed_id"], back_populates="followed")
+    following = relationship("Follower", foreign_keys=["Follower.follower_id"], back_populates="follower")
