cd594013a2a0e7ce4e9146f6f04efa5f15e71245
hamertonotieno99 <hamertonotieno99@gmail.com>
2025-12-07 10:17:59 +0000

    36th commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: './',
   plugins: [react()],
   server: {
     port: 3000,
@@ -13,12 +12,6 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
-    }
-  },
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: 'index.html'
     }
   }
 })
