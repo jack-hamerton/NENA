9a3933c2939bd49191308172a4594ee0f4d5f1af
hamertonotieno99 <hamertonotieno99@gmail.com>
2025-12-07 11:13:17 +0000

    41 commits

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,16 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ThemeProvider } from 'styled-components';
-import { xiaoGenshin } from './theme/xiaoGenshin';
-import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={xiaoGenshin}>
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
