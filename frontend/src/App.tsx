9dbe4c6c63a5013d25da779cdf03e0f763fd5f39
hamertonotieno99 <hamertonotieno99@gmail.com>
2025-12-07 09:51:50 +0000

    22

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,33 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { xiaoGenshin } from './theme/xiaoGenshin';
+import SplashScreen from './layout/SplashScreen/SplashScreen';
 import MainLayout from './layout/MainLayout';
+import FeedPage from './feed/FeedPage';
 import DiscoverPage from './discover/DiscoverPage';
 import UserProfile from './profile/UserProfile';
-import FeedPage from './feed/FeedPage';
 import MessagesPage from './messages/MessagesPage';
 import RoomsPage from './rooms/RoomsPage';
 import StudyPage from './study/StudyPage';
-import MusicPodcastPage from './pages/MusicPodcastPage';
-import PodcastHubPage from './pages/PodcastHubPage';
-import SplashScreen from './layout/SplashScreen/SplashScreen'; // Assuming you have this component
+import MusicPage from './music/MusicPage';
+import PodcastsPage from './podcasts/PodcastsPage';
 
 function App() {
   return (
     <ThemeProvider theme={xiaoGenshin}>
       <Router>
         <Routes>
-          <Route path="/" element={<MainLayout />}>
+          <Route path="/" element={<SplashScreen />} />
+          <Route path="/app" element={<MainLayout />}>
             <Route index element={<FeedPage />} />
             <Route path="discover" element={<DiscoverPage />} />
             <Route path="profile/:userId" element={<UserProfile />} />
             <Route path="messages" element={<MessagesPage />} />
             <Route path="rooms" element={<RoomsPage />} />
             <Route path="study" element={<StudyPage />} />
-            <Route path="music" element={<MusicPodcastPage />} />
-            <Route path="podcasts" element={<PodcastHubPage />} />
+            <Route path="music" element={<MusicPage />} />
+            <Route path="podcasts" element={<PodcastsPage />} />
           </Route>
-          <Route path="/splash" element={<SplashScreen />} />
         </Routes>
       </Router>
     </ThemeProvider>
---
+++ frontend/src/podcasts/PodcastsPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const PodcastsContainer = styled.div`
+  padding: 2rem;
+  color: ${props => props.theme.text};
+`;
+
+const PodcastsPage = () => {
+  return (
+    <PodcastsContainer>
+      <h1>Podcasts</h1>
+      <p>Here are some podcasts for you.</p>
+    </PodcastsContainer>
+  );
+};
+
+export default PodcastsPage;
