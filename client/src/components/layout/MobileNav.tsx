cd9a5000c90fa85718fbff7ef1ac33654ab14c41
kevwasonga <kevinwasonga116@gmail.com>
2026-03-09 15:01:05 +0300

    chore: update client/src/components/layout/MobileNav.tsx

--- client/src/components/layout/MobileNav.tsx
+++ client/src/components/layout/MobileNav.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { Home, Search, MessageSquare, Radio, Headphones, User } from "lucide-react";
+import { Home, Search, MessageSquare, Radio, User } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { useAuth } from "@/context/AuthContext";
 
